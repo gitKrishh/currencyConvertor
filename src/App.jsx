43e543287d0450f9bb656f7d07a88b85fc519899
gitkrishh <msgkrish192@gmail.com>
2025-06-15 15:45:40 +0530

    added input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
+import {InputBox} from './components'
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
-
-      <h1>Currency Converter</h1>
+      <div id="background">
+        <h1 id='heading'>Currency Converter</h1>
+      <InputBox
+      label = "from"
+      />
+      </div>
     </>
   )
 }
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,7 +1,36 @@
 import { useState,useRef, useEffect, useId } from "react";
+import './InputBox.css'
+
 
 function InputBox({
+    label,
+    amount,
+    onAmountChange
     
 }){
-
+    // console.log("InputBox rendered");
+    
+    const inputId = useId();
+    return(
+    <div id="mainInputBox">
+        <div id="from">
+            <label htmlFor={inputId}>
+                {label}
+            </label>
+            <input 
+            type="text" 
+            id={inputId}
+            placeholder="Amount"
+            value={amount}
+            onChange={(e)=> onAmountChange && onAmountChange(Number(e.target.value))}
+             />
+        </div>
+        <div id="chooseCurrency">
+            <label htmlFor="choosecurr">Currency Type</label>
+            <select id="selectInput"></select>
+        </div>
+    </div>
+    )
 }
+
+export default InputBox
