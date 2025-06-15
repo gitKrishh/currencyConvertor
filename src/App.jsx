b39ad60b8855d3d0574442bc75a46c28ff152cb5
gitkrishh <msgkrish192@gmail.com>
2025-06-15 17:56:09 +0530

    convert logic done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,52 @@
 import { useState } from 'react'
 import {InputBox} from './components'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 import './App.css'
 
 function App() {
+  const [amount, setamount] = useState(0)
+  const [from, setfrom] = useState("usd")
+  const [to, setto] = useState("inr")
+  const [converted, setconverted] = useState(0)
+  const currencyOptions = useCurrencyInfo(from)
+  const option = Object.keys(currencyOptions)
 
+  const convert = () => {
+    setconverted(amount * currencyOptions[to])
+  }
 
   return (
     <>
+    
       <div id="background">
         <h1 id='heading'>Currency Converter</h1>
       <InputBox
-      label = "from"
+      label = "From"
+      amount = {amount}
+      onAmountChange={(amount)=> setamount(amount)}
+      selectCurrency={from}
+      onCurrencyChange={(currency)=> setfrom(currency)}
+      currencyOptions={option}
       />
+      
+      
+      <InputBox
+      label = "To"
+      amount = {converted}
+      selectCurrency={to}
+      onCurrencyChange={(currency)=> setto(currency)}
+      currencyOptions={option}
+      />
+
+      <button 
+      id="convert"
+      onClick={convert}
+
+      >
+        Convert
+
+      </button>
+
       </div>
     </>
   )
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,35 +1,52 @@
-import { useState,useRef, useEffect, useId } from "react";
+import { useState, useRef, useEffect, useId } from "react";
 import './InputBox.css'
 
 
 function InputBox({
     label,
     amount,
-    onAmountChange
-    
-}){
+    onAmountChange,
+    selectCurrency,
+    onCurrencyChange,
+    amountDisable,
+    currencyOptions = []
+
+}) {
     // console.log("InputBox rendered");
-    
-    const inputId = useId();
-    return(
-    <div id="mainInputBox">
-        <div id="from">
-            <label htmlFor={inputId}>
-                {label}
-            </label>
-            <input 
-            type="text" 
-            id={inputId}
-            placeholder="Amount"
-            value={amount}
-            onChange={(e)=> onAmountChange && onAmountChange(Number(e.target.value))}
-             />
+
+
+    return (
+        <div id="mainInputBox">
+            <div id="from">
+                <label htmlFor="inputId">
+                    {label}
+                </label>
+                <input
+                    type="text"
+                    id="inputId"
+                    placeholder="Amount"
+                    value={amount}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                />
+            </div>
+            <div id="chooseCurrency">
+                <label htmlFor="choosecurr">Currency Type</label>
+                <select
+                    id="choosecurr"
+                    value={selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                >
+                    {currencyOptions.map((currency) => (
+                        <option
+                            value={currency}
+                            key={currency}
+                        >
+                            {currency}
+                        </option>
+                    ))}
+                </select>
+            </div>
         </div>
-        <div id="chooseCurrency">
-            <label htmlFor="choosecurr">Currency Type</label>
-            <select id="selectInput"></select>
-        </div>
-    </div>
     )
 }
 
